b40163877427b158b50299731766c7b58d5ddec9
ssduan <ssduan@ctrip.com>
2020-08-16 15:50:17 +0800

    promise

--- jsNote.js
+++ jsNote.js
@@ -1,5 +1,5 @@
 //
-a = 'aaa';
+a = "aaa";
 function foo() {
   console.log(this.a);
 }
@@ -7,11 +7,11 @@ foo();
 
 const obj = {
   a: 111,
-  b: 222,
+  b: 222
 };
 
-// 模拟call
-Function.prototype.myCall = function (context, ...args) {
+// 手写call
+Function.prototype.myCall = function(context, ...args) {
   context = (context ?? window) || new Object(context);
   const key = Symbol();
   context[key] = this;
@@ -22,8 +22,8 @@ Function.prototype.myCall = function (context, ...args) {
 
 foo.myCall(obj);
 
-// 模拟apply
-Function.prototype.myApply = function (context) {
+// 手写apply
+Function.prototype.myApply = function(context) {
   context = (context ?? window) || new Object(context);
   const key = Symbol();
   const args = arguments[1];
@@ -33,17 +33,17 @@ Function.prototype.myApply = function (context) {
   return result;
 };
 
-// 模拟bind
-Function.prototype.myBind = function (context) {
-  if (typeof this !== 'function') {
+// 手写bind
+Function.prototype.myBind = function(context) {
+  if (typeof this !== "function") {
     throw new Error(
-      'Function.prototype.bind - what is trying to be bound is not callable'
+      "Function.prototype.bind - what is trying to be bound is not callable"
     );
   }
   const self = this;
   const args = Array.prototype.slice.call(arguments, 1);
-  const fNOP = function () {};
-  const fBOUND = function () {
+  const fNOP = function() {};
+  const fBOUND = function() {
     console.log(this, self);
     const bindArgs = Array.prototype.slice.call(arguments);
     return self.apply(
@@ -60,13 +60,13 @@ Function.prototype.myBind = function (context) {
 const fBind = foo.myBind(obj);
 const objBind = new fBind();
 
-// 模拟new
+// 手写new
 function objFactory() {
   const obj = new Object();
   Constructor = Array.prototype.shift.call(arguments);
   obj._proto_ = Constructor.prototype;
   const result = Constructor.apply(obj, arguments);
-  return typeof result === 'object' ? result : obj;
+  return typeof result === "object" ? result : obj;
 }
 
 function Factory(name, age) {
@@ -75,18 +75,18 @@ function Factory(name, age) {
   this.name = name;
   this.age = age;
   return {
-    a: 'a',
-    b: 'b',
+    a: "a",
+    b: "b"
   };
 }
 
-const factoryObj = objFactory(Factory, 'lily', 18);
+const factoryObj = objFactory(Factory, "lily", 18);
 console.log(factoryObj);
 
 // 防抖函数
 function debounce(fn, wait, immediate) {
   let timeout = null;
-  const debounced = function () {
+  const debounced = function() {
     const self = this;
     const args = arguments;
     let result;
@@ -105,21 +105,21 @@ function debounce(fn, wait, immediate) {
     }
     return result;
   };
-  debounced.cancel = function () {
+  debounced.cancel = function() {
     clearTimeout(timeout);
     timeout = null;
   };
   return debounced;
 }
 document.addEventListener(
-  'click',
+  "click",
   debounce(() => console.log(123), 1000, true)
 );
 
 // 节流函数
 function throttle(fn, wait) {
   let previous = 0;
-  return function () {
+  return function() {
     const self = this;
     const args = arguments;
     let now = +new Date();
@@ -132,7 +132,7 @@ function throttle(fn, wait) {
   };
 }
 document.addEventListener(
-  'click',
+  "click",
   throttle(() => console.log(111), 1000)
 );
 
@@ -147,218 +147,261 @@ JSON.parse(JSON.stringify(arr)); //可以拷贝引用型数组，但不能拷贝
 
 // 深拷贝函数
 function deepClone(obj) {
-  if (typeof obj !== 'object') {
+  if (typeof obj !== "object") {
     return;
   }
   let cloneObj = Array.isArray(obj) ? [] : {};
   for (let key in obj) {
     cloneObj[key] =
-      typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key];
+      typeof obj[key] === "object" ? deepClone(obj[key]) : obj[key];
   }
   return cloneObj;
 }
 
 // 原型和原型链
 function Person() {
-  this.name = 'jack';
+  this.name = "jack";
   this.age = 18;
 }
-Person.prototype.sex = 'male';
+Person.prototype.sex = "male";
 
 const person = new Person();
 person.name; //jack
 person.sex; //male
 
-person._proto_ === Person.prototype;   //true
-Person.prototype.constructor === Person;    //true
-Person.prototype._proto_ === Object.prototype;   // true
-Object.prototype.constructor === Object;   // true
-Object.prototype._proto_ === null;   // true
+person._proto_ === Person.prototype; //true
+Person.prototype.constructor === Person; //true
+Person.prototype._proto_ === Object.prototype; // true
+Object.prototype.constructor === Object; // true
+Object.prototype._proto_ === null; // true
 
 // 创建对象的多种方式及优缺点
 // 1. 工厂模式；
 // 缺点：对象无法识别，因为所有的实例都指向一个原型
-function createPerson(name,age){
+function createPerson(name, age) {
   let person = new Object();
   person.name = name;
   person.age = age;
-  person.sayHi = function(){
-    console.log('hi');
-  }
+  person.sayHi = function() {
+    console.log("hi");
+  };
   return person;
 }
 // 2. 构造函数模式；
 // 优点：实例可以识别为一个特定的类型
 // 缺点：每次创建实例时，每个方法都要被创建一次
-function Person(name,age){
+function Person(name, age) {
   this.name = name;
   this.age = age;
-  this.sayHi = function(){
-    console.log('hi');
-  }
+  this.sayHi = function() {
+    console.log("hi");
+  };
 }
-const jack = new Person('jack', 20);
+const jack = new Person("jack", 20);
 
 // 3. 原型模式；
 // 优点：方法不会每次都被创建
 // 缺点：所有的属性和方法都可以共享，不能初始化参数
-function Person(name){};
+function Person(name) {}
 Person.prototype.name = this.name;
-Person.prototype.getName = function(){
-  console.log(this.name)
-}
-const rose = new Person('rose');
-rose.getName();  //空字符串  不能初始化参数
+Person.prototype.getName = function() {
+  console.log(this.name);
+};
+const rose = new Person("rose");
+rose.getName(); //空字符串  不能初始化参数
 
 // 4. 原型模式优化
 // 优点：能传参，可控制私有属性和共享属性
 // 缺点：封装性不太好
-function Person(name, age){
+function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 Person.prototype = {
-  constructor:Person,
-  sex:'male',
-  getName: function(){
-    console.log(this.name)
+  constructor: Person,
+  sex: "male",
+  getName: function() {
+    console.log(this.name);
   }
-}
-const lily = new Person('lily', 20);
+};
+const lily = new Person("lily", 20);
 lily.getName();
 
 // 5. 原型模式更好封装
 // 缺点：不能使用字面量赋值prototype
-function Person(name, age){
+function Person(name, age) {
   this.name = name;
   this.age = age;
-  if(typeof(this.getName) !== 'function'){
+  if (typeof this.getName !== "function") {
     Person.prototype = {
       constructor: Person,
-      sex:'male',
-      getName: function(){
+      sex: "male",
+      getName: function() {
         console.log(this.name);
       }
-    }
+    };
   }
 }
 // 改写为
-function Person(name, age){
+function Person(name, age) {
   this.name = name;
   this.age = age;
-  if(typeof(this.getName) !== 'function'){
+  if (typeof this.getName !== "function") {
     Person.prototype = {
       constructor: Person,
-      sex:'male',
-      getName: function(){
+      sex: "male",
+      getName: function() {
         console.log(this.name);
       }
-    }
-    return new Person();  //加一行
+    };
+    return new Person(); //加一行
   }
 }
-
 
 // 继承的多种方式及优缺点
 // 1. 原型链继承
 // 缺点：所有的属性和方法都共享，引用类型, 不能向父级传参
-function Parent(){
-  this.name = 'jack';
-  this.hobby = ['a', 'b']
-  this.getName = function(){
+function Parent() {
+  this.name = "jack";
+  this.hobby = ["a", "b"];
+  this.getName = function() {
     console.log(this.name);
-  }
+  };
 }
 
-function Child(){
-  this.name = 'rose';
+function Child() {
+  this.name = "rose";
 }
 Child.prototype = new Person();
 let child1 = new Child();
-child1.hobby.push('c');
+child1.hobby.push("c");
 let child2 = new Child();
-child1.hobby;   // ['a', 'b', 'c']
-child2.hobby;   // ['a', 'b', 'c']  都改变了
+child1.hobby; // ['a', 'b', 'c']
+child2.hobby; // ['a', 'b', 'c']  都改变了
 
 // 2. 构造函数继承
 // 优点：改变实例的属性不影响父级属性，可以在 Child 中向 Parent 传参
 // 缺点：方法都在构造函数中定义，每次创建实例都会创建一遍方法。
-function Parse(name){
+function Parse(name) {
   this.name = name;
-  this.hobby = [1,2,3];
-  this.getName = function(){
+  this.hobby = [1, 2, 3];
+  this.getName = function() {
     console.log(this.name);
-  }
+  };
 }
 
-function Child(name){
+function Child(name) {
   Parse.call(this, name);
 }
 
-let child1 = new Child('jack');
-let child2 = new Child('rose');
-
+let child1 = new Child("jack");
+let child2 = new Child("rose");
 
 // 3. 组合继承（原型链+构造函数）
 // 优点：融合原型链继承和构造函数的优点，是 JavaScript 中最常用的继承模式。
-function Parent(name, age){
+function Parent(name, age) {
   this.name = name;
   this.age = age;
 }
-Parent.prototype.getName = function(){
+Parent.prototype.getName = function() {
   console.log(this.name);
-}
+};
 
-function Child(name, age){
+function Child(name, age) {
   Parent.call(this, name, age);
 }
 Child.prototype = new Parent();
 Child.prototype.constructor = Child;
 
-let child1 = new Child('child1', 20);
-let child2 = new Child('child2', 22);
+let child1 = new Child("child1", 20);
+let child2 = new Child("child2", 22);
 
 // 寄生组合继承
-function createObj(o){
-  function F(){};
+function createObj(o) {
+  function F() {}
   F.prototype = o;
   return new F();
 }
 
-function prototype(Child, Parent){
+function prototype(Child, Parent) {
   const prototype = createObj(Parent);
   prototype.constructor = Child;
   Child.prototype = prototype;
 }
 
-
 // JS类型转换
 // https://github.com/mqyqingfeng/Blog/issues/159
 
-
 // 类型判断
 // typeof   不能判断数组、json等对象类型
-typeof('')   //string
-typeof(1)    //number
-typeof(true)   //boolean
-typeof(Symbol())   //symbol
-typeof(null)    //object
-typeof(undefined)   //undefined
-typeof([])   //object
-typeof({})   //object 
-typeof(function(){})   //function
-typeof(new Date())    //object
-
+typeof ""; //string
+typeof 1; //number
+typeof true; //boolean
+typeof Symbol(); //symbol
+typeof null; //object
+typeof undefined; //undefined
+typeof []; //object
+typeof {}; //object
+typeof function() {}; //function
+typeof new Date(); //object
 
 // Object.prototype.toString();
-Object.prototype.toString.call(new Date())   //object Date
+Object.prototype.toString.call(new Date())(
+  //object Date
 
-// instanceof：用于检测构造函数的 prototype 属性是否出现在某个实例对象的原型链上。
-([] instanceof Array)   // true
-
+  // instanceof：用于检测构造函数的 prototype 属性是否出现在某个实例对象的原型链上。
+  [] instanceof Array
+); // true
 
 // 数组排序
-// 1. 数组方法
-function arrSort(arr){
-  
+// 1. 利用数组自带sort方法
+function sortArray(arr) {
+  const newArr = arr.slice(0);
+  if (!Array.isArray(newArr)) {
+    return;
+  }
+  newArr.sort((a, b) => {
+    return a - b;
+  });
+  return newArr;
 }
+
+// 2. 冒泡排序算法
+// 当前项和后一项进行比较 如果当前项大于后一项则 交换位置
+function bubbleSort(arr) {
+  const newArr = arr.slice(0);
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; j < newArr.length; j++) {
+      if (newArr[j] > newArr[j + 1]) {
+        const temp = newArr[j];
+        newArr[j] = newArr[j + 1];
+        newArr[j + 1] = temp;
+      }
+    }
+  }
+  return newArr;
+}
+
+// 3.快速排序算法
+// 创建两个数组(left right) 用中间项和其它项比较，比中间项小的放在左边数组 比中间项大的放在右边数组...
+// 左边数组和右边数组均按照以上思路 进行排序
+function quickSort(arr) {
+  const newArr = arr.slice(0);
+  if (newArr.length <= 1) {
+    return newArr;
+  }
+  const midIndex = Math.floor(newArr.length / 2);
+  const mid = newArr.splice(midIndex, 1);
+  const leftArr = [];
+  const rightArr = [];
+  for (let i = 0; i < newArr.length; i++) {
+    newArr[i] < mid ? leftArr.push(newArr[i]) : rightArr.push(newArr[i]);
+  }
+  return quickSort(leftArr).concat(mid, quickSort(rightArr));
+}
+
+// Promise
+// 见promise.js文件
+
+
+
---
+++ promise.js
@@ -0,0 +1,164 @@
+// promise A+的三种状态: pending, fulfilled, rejected
+
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
+
+class MyPromise {
+  constructor(executer) {
+    // console.log('执行了')
+    this._status = PENDING;
+    this._value = undefined;
+    this.resolveQueue = [];
+    this.rejectQueue = [];
+
+    const _resolve = val => {
+      const run = () => {
+        if (this._status !== PENDING) {
+          return;
+        }
+        this._status = FULFILLED;
+        this._value = val;
+        while (this.resolveQueue.length) {
+          const cb = this.resolveQueue.shift();
+          cb(val);
+        }
+      };
+      setTimeout(run);
+    };
+
+    const _reject = val => {
+      const run = () => {
+        if (this._status !== PENDING) {
+          return;
+        }
+        this._status = REJECTED;
+        this._value = val;
+        while (this.rejectQueue.length) {
+          const cb = this.rejectQueue.shift();
+          cb(val);
+        }
+      };
+      setTimeout(run);
+    };
+
+    executer(_resolve, _reject);
+  }
+
+  then(resolveFn, rejectFn) {
+    return new MyPromise((resolve, reject) => {
+      const fulfilledFn = val => {
+        typeof resolveFn !== "function" && (resolveFn = val => val);
+        typeof rejectFn !== "function" &&
+          (rejectFn = reason => {
+            throw new Error(reason instanceof Error ? reason.message : reason);
+          });
+        // 实现链式调用
+        try {
+          const x = resolveFn(val);
+          x instanceof MyPromise ? x.then(resolve, reject) : resolve(x);
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      const rejectedFn = val => {
+        // 实现链式调用
+        try {
+          const x = rejectFn(val);
+          x instanceof MyPromise ? x.then(resolve, reject) : resolve(x);
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      switch (this._status) {
+        case PENDING:
+          this.resolveQueue.push(fulfilledFn);
+          this.rejectQueue.push(rejectedFn);
+          break;
+        case FULFILLED:
+          fulfilledFn(this._value);
+        case REJECTED:
+          rejectedFn(this._value);
+      }
+    });
+  }
+
+  catch(rejectFn) {
+    return this.then(undefined, rejectFn);
+  }
+
+  static resolve(val) {
+    return val instanceof MyPromise
+      ? val
+      : new MyPromise((resolve, reject) => resolve(val));
+  }
+
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => reject(reason));
+  }
+
+  finally(callback) {
+    return this.then(
+      res => MyPromise.resolve(callback()).then(() => res),
+      err =>
+        MyPromise.resolve(callback()).then(() => {
+          throw err;
+        })
+    );
+  }
+
+  all(promiseArr){
+    if(!Array.isArray(promiseArr)){
+      throw new Error('不是数组')
+    }
+    let index = 0;
+    let result = [];
+    return new MyPromise((resolve, reject) => {
+      promiseArr.forEach((p, i) => {
+        
+      })
+    })
+  }
+}
+
+const p2 = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("result-p2");
+  }, 2000);
+  setTimeout(() => {
+    reject("erroe-p2");
+  }, 1000);
+});
+
+p2.finally();
+
+// 全面测试
+const p3 = new MyPromise((resolve, reject) => {
+  resolve(1); //同步executor测试
+});
+
+p3.then(res => {
+  console.log(res);
+  return 2; //链式调用测试
+})
+  .then() //值穿透测试
+  .then(res => {
+    console.log(res);
+    return new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(3); //返回Promise测试
+      }, 1000);
+    });
+  })
+  .then(res => {
+    console.log(res);
+    throw new Error("reject测试"); //reject测试
+  })
+  .then(
+    () => {},
+    err => {
+      console.log(err);
+    }
+  );
